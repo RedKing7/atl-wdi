ffb030621725769760b040edb023a98c961c7e36
Rory Johnson <redkjohnson@gmail.com>
2017-09-06 14:33:08 -0400

    finished independent-practice-starter.js

--- labs/javaScript/js-independent-practice/independent-practice-starter.js
+++ labs/javaScript/js-independent-practice/independent-practice-starter.js
@@ -2,7 +2,15 @@
 // an array of strings. The function should return an array of numbers where each
 // number is the length of the corresponding string.
 
-// your code here
+const lengths = (arr) =>{
+    lengthArr = [];
+    arr.forEach(function(element) {
+        lengthArr.push(element.length);
+    }, this);;
+    return lengthArr;
+}
+var words = ["hello", "what", "is", "up", "dude"]
+////console.log(lengths(words));
 
 // 2. Write a Javascript function called `transmogrifier`
 // This function should accept three arguments, which you can assume will be numbers.
@@ -11,10 +19,17 @@
 // The transmogrified result of three numbers is the product of the first two numbers,
 // raised to the power of the third number.
 
-// your code here
+const transmogrifier = (a,b,c) =>{
+    return (a * b) ** c;
+}
+////console.log(transmogrifier(5,3,2))
 
 // 3. Write a function `wordReverse` that accepts a single argument, a string. The
 // method should return a string with the order of the words reversed. Don't worry
 // about punctuation.
 
-// your code here
+const wordReverse = (str) =>{
+    return str.split(' ').reverse().join(' ');
+}
+let phrase = "Now I know what a TV dinner feels like";
+//console.log(wordReverse(phrase));
