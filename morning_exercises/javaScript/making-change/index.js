e99e4365bc2eb8d56388e0c982db5ffb33c6f3b1
Rory Johnson <redkjohnson@gmail.com>
2017-09-08 09:44:19 -0400

    finished morning exercise 'making change'

--- morning_exercises/javaScript/making-change/index.js
+++ morning_exercises/javaScript/making-change/index.js
@@ -3,10 +3,32 @@
 // See LICENSE for details.
 "use strict";
 /// DO NOT EDIT ABOVE THIS LINE ///
-
+//[qu,di,ni,pe]
 var makeChange = function(price, payment) {
-  // Your Code Here
+  if(price > payment)
+    return [0,0,0,0];
+  let den = arguments[2] || [25,10,5,1];
+  let left = Math.round((payment - price)*100);
+
+  let answer = [];
+  den.forEach(function(coin) {
+    let numCoins = Math.floor(left / coin);
+    left -= coin * numCoins;
+    answer.push(numCoins);
+  });
+  return answer;
+/*
+  let quarters = Math.floor(cents/25);
+  cents = cents - (quarters * 25);
+  let dimes = Math.floor(cents/10);
+  cents = cents - (dimes * 10);
+  let nickels = Math.floor(cents/5);
+  cents = cents - (nickels * 5);
+  return [quarters, dimes, nickels, cents];
+*/
 };
+
+console.log(makeChange(5.39, 6.00, [25, 10, 5, 1]));
 
 /// DO NOT EDIT BELOW THIS LINE ///
 module.exports = {
