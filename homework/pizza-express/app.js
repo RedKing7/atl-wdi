1c68ea2270b9ecfc674a2f72980f851872c2d82f
Rory Johnson <redkjohnson@gmail.com>
2017-09-19 16:22:04 -0400

    completed GET routes

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -4,6 +4,17 @@ var app = express();
 
 var port = 3000;
 
+app.get('/', (req, res)=>{
+   res.send('Welcome to Pizza Express!');
+})
+
+app.get('/topping/:type', (req, res)=>{
+   res.send(`${req.params.type} pizza! Good choice.`);
+})
+
+app.get('/order/:amount/:size', (req, res)=>{
+   res.send(`Your order for ${req.params.amount} ${req.params.size} pizzas will be ready in 1 minute! Cuz' we're stupid fast!`);
+})
 
 app.listen(port, ()=>{
    console.log("==========================")
