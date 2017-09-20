f15917d91ae86be5306cf6f77abcb5f52fa73b3f
Rory Johnson <redkjohnson@gmail.com>
2017-09-20 13:56:04 -0400

    set up server.js

--- homework/express/express-pirates-create-read/pirates-wiki/server.js
+++ homework/express/express-pirates-create-read/pirates-wiki/server.js
@@ -1,1 +1,22 @@
-//your code here
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+app.use(express.static(__dirname + '/public'));
+
+const port = 3000;
+
+/*app.get('/pirates', (req, res)=>{
+   res.render('pirates', {
+
+   });
+});*/
+
+app.listen(port, ()=>{
+   console.log("==========================")
+   console.log('LISTENING ON PORT ' + port);
+   console.log("==========================")
+})
