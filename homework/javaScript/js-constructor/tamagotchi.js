028df14007a438aa9b054a3333e53174a2a5b36d
Rory Johnson <redkjohnson@gmail.com>
2017-09-21 16:05:25 -0400

    part 3: added new methods

--- homework/javaScript/js-constructor/tamagotchi.js
+++ homework/javaScript/js-constructor/tamagotchi.js
@@ -14,10 +14,26 @@ tamagotchi.prototype.cry = function(){
    console.log(this.name, 'WAHH!!');
 };
 
+tamagotchi.prototype.puke = function(){
+   this.foodInTummy -= 1;
+   console.log('food in tummy: ' + this.foodInTummy, '*sob*');
+};
+
+tamagotchi.prototype.yawn = function(){
+   this.restedness -= 1;
+   console.log(`${this.name} has current restedness of: ${this.restedness}`);
+};
+
 //create new Tamagotchis
 let t1 = new tamagotchi('Tom', 'cat');
 let t2 = new tamagotchi('Jerry', 'mouse');
 
+let t3 = new tamagotchi('Steve', 'rock');
+let t4 = new tamagotchi('Doug', 'dog');
+
 //test out your Tamagotchies below via console.logs
 t1.cry();
 t2.cry();
+
+t3.puke();
+t4.yawn();
