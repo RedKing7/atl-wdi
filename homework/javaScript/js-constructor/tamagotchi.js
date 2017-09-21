b8584b0e7ea2dbd665fc36957f0274d6aad32b37
Rory Johnson <redkjohnson@gmail.com>
2017-09-21 15:57:04 -0400

    part 1: created tamagotchi constructor

--- homework/javaScript/js-constructor/tamagotchi.js
+++ homework/javaScript/js-constructor/tamagotchi.js
@@ -1,9 +1,21 @@
 console.log('tamagotchi file is loaded');
 
 //your class declaration here
+var tamagotchi = function(){
+   this.foodInTummy = 10;
+   this.restedness = 10;
+   this.health = 10;
+};
 
+tamagotchi.prototype.cry = function(){
+   this.foodInTummy -= 1;
+   console.log('WAHH!!');
+};
 
 //create new Tamagotchis
-
+let t1 = new tamagotchi();
+let t2 = new tamagotchi();
 
 //test out your Tamagotchies below via console.logs
+t1.cry();
+t2.cry();
