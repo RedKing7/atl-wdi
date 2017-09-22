ab35dc9ee931f2e6d232b92d934ef92be30f9b41
Rory Johnson <redkjohnson@gmail.com>
2017-09-22 16:36:33 -0400

    finished promise-burger

---
+++ homework/javaScript/promise-it-wont-hurt/program.js
@@ -0,0 +1,37 @@
+//Warm up
+//setTimeout(()=>{console.log('TIMED OUT!')}, 300);
+
+//fulfill a promise
+// let promise = new Promise(function(fulfill, reject){
+//    setTimeout(()=>{
+//       fulfill('FULFILLED!');
+//    }, 300);
+// })
+
+// promise.then(console.log)
+
+//reject a promise
+//'use strict';
+// let promise = new Promise((fulfill, reject)=>{
+//    setTimeout(()=>{
+//       let error = new Error('REJECTED!');
+//       reject(error);
+//    }, 300)
+// })
+// function onReject(error){
+//    console.log(error.message);
+// }
+// promise.catch(function(error){
+//       onReject(error);
+// })
+
+//to reject or not to reject
+let promise = new Promise((fulfill, reject)=>{
+   fulfill('I FIRED');
+   reject(new Error('I DID NOT FIRE'));
+})
+let onRejected = function(error){
+   console.log(error.message);
+}
+promise.then(console.log)
+       .catch(onRejected);
--- labs/javaScript/promise-burger/hw.js
+++ labs/javaScript/promise-burger/hw.js
@@ -7,7 +7,7 @@ const kitchen = require('./kitchen');
 // Call this method and console.log the data that comes back when the Promise is resolved.
 
 //CODE FOR QUESTION 1 HERE
-
+kitchen.readMenu().then(console.log);
 
 // Order Some Food
 /* *************************************************************** */
@@ -16,14 +16,14 @@ const kitchen = require('./kitchen');
 //  Console.log the data that comes back about this burger.
 
 // CODE FOR QUESTION 2 HERE
-
+kitchen.order('holy guacamole').then(console.log);
 
 // 3. Try and use the same method to order a `quesadilla`.  
 //  Since Promise Burger only serves burgers, this will throw an error.  
 //  Make sure that your Promise can be dealt with when it is rejected.
 
 // CODE FOR QUESTION 3 HERE
-
+kitchen.order('quesadilla').then(console.log).catch(console.log);
 
 // Add A Burger To The Menu
 /* *************************************************************** */
@@ -41,10 +41,13 @@ const newBurger = {
 }
 
 // CODE FOR QUESTION 4 HERE
+kitchen.addToMenu(newBurger).then(console.log).catch(console.log);
 
-// 5. Validate that the new item has been added to the menu by calling `readMenu` again.  
+// 5. Validate that the new item has been added to the menu by calling `readMenu` again.
 // We only want to call `readMenu` after we get a response that is successful. 
 // Remember that we can chain promises together the same way that we can chain jQuery methods.
+
+kitchen.readMenu().then(console.log).catch(console.log);
 
 // REFACTOR QUESTION 4 TO ACCOUNT FOR THIS REQUIREMENT
 
@@ -74,3 +77,8 @@ const customers = [
 ]
 
 // CODE FOR QUESTION 6 HERE
+kitchen.order(customers[0].order)
+  .then(kitchen.order(customers[1].order)
+  .then(kitchen.order(customers[2].order)
+  .then(kitchen.order(customers[3].order)
+  .then(console.log('All Orders Filled')))))
