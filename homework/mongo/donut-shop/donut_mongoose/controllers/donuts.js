31ee620925a0590567fe335f1b472b1613fc533f
Rory Johnson <redkjohnson@gmail.com>
2017-09-26 23:13:10 -0400

    successfully seeded Donut data

--- homework/mongo/donut-shop/donut_mongoose/controllers/donuts.js
+++ homework/mongo/donut-shop/donut_mongoose/controllers/donuts.js
@@ -11,21 +11,27 @@ var Donut = require("../models/donuts.js");
 // INDEX
 //======================
 // Create a GET index route "/" that sends all donuts to index.hbs
-
+router.get('/', (req,res)=>{
+   res.send('Hi!');
+})
 
 
 //======================
 // NEW
 //======================
 // Create a GET new route "/new" that renders the new.hbs form
-
+router.get('/', (req,res)=>{
+   
+})
 
 
 //======================
 // SHOW
 //======================
 // Create a GET show route "/:id" that renders the donut's show page
-
+router.get('/', (req,res)=>{
+   
+})
 
 
 
@@ -34,7 +40,9 @@ var Donut = require("../models/donuts.js");
 //======================
 // Create a POST index route "/" that creates a new donut
 // and upon success redirects back to the index page "/"
-
+router.get('/', (req,res)=>{
+   
+})
 
 
 //======================
@@ -42,7 +50,9 @@ var Donut = require("../models/donuts.js");
 //======================
 // Create a GET edit route "/:id/edit" that renders the edit.hbs page and
 // sends that donut's data to it
-
+router.get('/', (req,res)=>{
+   
+})
 
 
 //======================
@@ -50,7 +60,9 @@ var Donut = require("../models/donuts.js");
 //======================
 // Create a PUT update route "/:id" that updates the donut and
 // redirects back to the SHOW PAGE (not index)
-
+router.get('/', (req,res)=>{
+   
+})
 
 
 //======================
@@ -58,7 +70,9 @@ var Donut = require("../models/donuts.js");
 //======================
 // Create a DELETE delete route "/:id" that deletes the donut and
 // redirects back to index page "/"
-
+router.get('/', (req,res)=>{
+   
+})
 
 
 //======================
--- homework/mongo/donut-shop/donut_mongoose/models/donuts.js
+++ homework/mongo/donut-shop/donut_mongoose/models/donuts.js
@@ -1,6 +1,5 @@
 //requirements: require mongoose
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/students');
 
 mongoose.Promise = global.Promise;
 //create your donut schema:
@@ -23,6 +22,4 @@ const DonutSchema = new Schema({
 const DonutModel = mongoose.model('Donut', DonutSchema);
 
 //export your donut with module.exports()
-module.exports = {
-   DonutModel: DonutModel
-}
+module.exports = DonutModel
