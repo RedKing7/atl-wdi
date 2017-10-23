596ba84f14425ecc8766b09d15e6314893cd1fc7
Rory Johnson <redkjohnson@gmail.com>
2017-10-23 13:31:54 -0400

    2. Nil skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -1,3 +1,5 @@
+=begin
+
 #challenge 1
 #1
 a = true
@@ -7,3 +9,13 @@ b = false
 puts a && b
 #4
 puts a && ! b
+
+=end
+
+#challenge 2
+#1
+nope = nil
+puts nope
+puts "#{nope}nil was here"
+print nope
+puts nope.class
