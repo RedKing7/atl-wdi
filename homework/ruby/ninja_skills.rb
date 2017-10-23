bc3840ab5319914aba5f81b90747aa9f2092b6b7
Rory Johnson <redkjohnson@gmail.com>
2017-10-23 13:40:31 -0400

    4. String skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -19,13 +19,31 @@ puts "#{nope}nil was here"
 print nope
 puts nope.class
 
-=end
 
 #Challenge 3
 #1
 d = 10
 puts d
+#2
 e = 5.3
 puts e
+#3
 d = e
 puts d
+
+=end
+#Challenge 4
+#1
+christine = 'Hi, I\'m Christine'
+#2
+likes_to = 'long walks on the beach, kinda.'
+#3
+tims = "#{christine} and I like #{likes_to}"
+puts tims
+#4
+g = '2'
+#5
+g_to_number = 2
+#6
+#puts g + g_to_number  '+' isn't method of string class
+puts g + g_to_number.to_s
